6235142c59fe6abe3d1eb924d25621ac626274fa
Dan Butcher <danbobbutcher@gmail.com>
2023-01-06 09:30:56 -0700

    simple test api end point works

--- server/src/server/flaskServer.py
+++ server/src/server/flaskServer.py
@@ -1,3 +1,4 @@
+import flask.json
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/api/test", methods=['GET'])
+def test_api():
+    test_obj = {
+        "test1": "test"
+    }
+    return flask.json.jsonify(test_obj)
