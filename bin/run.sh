7348efa936551abb2a8ff56839be07a1e59bb717
Dan <danbobbutcher@gmail.com>
2023-01-24 09:32:07 -0700

    building run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 # run this script from the root of the repo
-cd client && npm run dev
+
+MONGO_IMAGE="mongo:latest"
+
+check_docker_container_running
+{
+  echo "Checking for "
+}
+
+acquire_current_mongo()
+{
+  echo "Checking for latest mongo Docker image: $MONGO_IMAGE"
+  if [ "$( docker image inspect $MONGO_IMAGE | grep 'Error: No such image:')" != "" ]; then
+    echo "$MONGO_IMAGE not found, pulling..."
+    docker pull $MONGO_IMAGE
+  else
+    echo "$MONGO_IMAGE found."
+  fi
+}
+
+check_for_docker()
+{
+  echo "Checking for docker."
+  if [ "$( type docker )" != "" ]; then
+    echo "docker not found, please install docker."
+    exit 1
+  else
+    echo "docker found"
+  fi
+}
+
+
+# RUN
+echo "Running $0"
+check_for_docker
+acquire_current_mongo
+#cd client && npm run dev
