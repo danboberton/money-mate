18cb8a765365fc028c50a3a9d2c9a8fa4b4eb844
Andrew Derr <andrewderr2019@gmail.com>
2023-03-09 09:29:14 -0700

    Update PlayWright tests

--- client/src/tests/Playwright/App.test.ts
+++ client/src/tests/Playwright/App.test.ts
@@ -16,9 +16,8 @@ test('Test that the Budget button on the homepage can be clicked and Headers are
   await page.getByRole('button', { name: 'Budget' }).click();
 
   await expect(page.getByRole('heading', { name: 'Budget View' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'BudgetSettings' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Budget Analysis' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Transaction Table' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Budget Info' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Transactions' })).toBeVisible();
 });
 
 test('Test that the Menu can be opened and closed, and the correct options are visible', async ({ page }) => {
@@ -34,15 +33,23 @@ test('Test that the Menu can be opened and closed, and the correct options are v
   await page.getByRole('button', { name: 'Close Menu' }).click();
 });
 
-test('Test that the Menu can be opened and budget can be accessed', async ({ page }) => {
+test('test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-
   await page.getByRole('button', { name: 'Open Menu' }).click();
-
   await page.getByRole('menuitem', { name: 'Budget' }).click();
-
+  
   await expect(page.getByRole('heading', { name: 'Budget View' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'BudgetSettings' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Budget Analysis' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Transaction Table' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Budget Info' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Transactions' })).toBeVisible();
+});
+
+test('Test that budget page has Transactions section', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Budget' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Transactions' })).toBeVisible();
+  await expect(page.getByRole('columnheader', { name: 'Date' })).toBeVisible();
+  await expect(page.getByRole('columnheader', { name: 'Time' })).toBeVisible();
+  await expect(page.getByRole('columnheader', { name: 'Amount' })).toBeVisible();
+  await expect(page.getByRole('row', { name: 'Date Time Amount Category' }).getByRole('columnheader', { name: 'Category' })).toBeVisible();
 });
