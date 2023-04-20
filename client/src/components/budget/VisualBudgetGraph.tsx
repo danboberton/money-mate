05a0a0386aa259c3c54f39ac88aadf1892af670f
Dan <danbobbutcher@gmail.com>
2023-04-20 09:34:07 -0600

    Added percentage to tool tip formatting close #34

--- client/src/components/budget/VisualBudgetGraph.tsx
+++ client/src/components/budget/VisualBudgetGraph.tsx
@@ -1,24 +1,28 @@
-import { Doughnut } from 'react-chartjs-2';
+import {Doughnut} from 'react-chartjs-2';
 import {BudgetOutcome_t} from "./BudgetOutcome";
-import {Chart, ArcElement, Tooltip, Legend} from 'chart.js'
+import {Chart, ArcElement, Tooltip, Legend, ScaleChartOptions, ChartData, ChartOptions} from 'chart.js'
+
 Chart.register(ArcElement, Tooltip, Legend);
 
+interface PieProps {
+    options: ChartOptions<'doughnut'>;
+    data: ChartData<'doughnut'>;
+}
 
+export default function GeneratePieGraph(props: { totalExpense: number, totalUncategorized: number, outcomes: Array<BudgetOutcome_t>, totalIncome: number }) {
 
-export default function GeneratePieGraph(props: {totalExpense: number, totalUncategorized: number, outcomes: Array<BudgetOutcome_t>, totalIncome: number}){
-    
-    let categories:string[] = [];
+    let categories: string[] = [];
     let outcomePercentage = [];
     //Iterate through budgetOutcomes 
 
-    for(let i of props.outcomes){
+    for (let i of props.outcomes) {
         //to get spent (vs earned)
-        if(i.outcome > 0){
+        if (i.outcome > 0) {
             //If category exists and isn't a salary (earned money)                  //there must be a better way to check for salary
-            if(i.category != undefined && !categories.includes(i.category) && i.category != "Regular Salary"){
+            if (i.category != undefined && !categories.includes(i.category) && i.category != "Regular Salary") {
                 categories.push(i.category);
                 // Calculate how much of the total income was spent toward a specific category in % and add to its own array
-                outcomePercentage.push(Math.round((i.outcome/props.totalExpense) * 100));
+                outcomePercentage.push(Math.round((i.outcome / props.totalExpense) * 100));
             }
         }
     }
@@ -38,7 +42,7 @@ export default function GeneratePieGraph(props: {totalExpense: number, totalUnca
         labels: categories,
         datasets: [
             {
-                label: ' Spent',
+                label: ' % Spent',
                 data: outcomePercentage,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
@@ -47,22 +51,33 @@ export default function GeneratePieGraph(props: {totalExpense: number, totalUnca
                     'rgba(75, 192, 192, 0.2)',
                     'rgba(153, 102, 255, 0.2)',
                     'rgba(255, 159, 64, 0.2)',
-                  ],
-                  borderColor: [
+                ],
+                borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
                     'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)',
-                  ],
+                ],
             }
         ],
     };
+    const options: ChartOptions<'doughnut'> = {
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: (item) =>
+                        ` ${item.formattedValue}%`,
+                },
+            },
+        },
+    }
 
-    return( 
-        <Doughnut   data={data}
-                    
+    return (
+        <Doughnut
+            data={data}
+            options={options}
         />
     );
 }
