cf0b6327e3559dd85730e47953e931dfbc7cd404
ernman37 <Rion.duckworth@gmail.com>
2023-04-22 18:40:46 -0600

    add add new transaction method

--- client/src/components/budget/TransactionTable.tsx
+++ client/src/components/budget/TransactionTable.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import { Box, DataTable, CardFooter, CardHeader, Heading, Page, Layer, Header, Form, FormField, PageContent, Card, CardBody, Button } from "grommet";
+import { Box, Select, DateInput, TextInput, DataTable, CardFooter, CardHeader, Heading, Page, Layer, Header, Form, FormField, PageContent, Card, CardBody, Button } from "grommet";
 import { Filter } from "grommet-icons";
 import { Transaction_t } from "./Transaction";
+import { GetMonthResponse_t } from "./GetMonthResponse";
+import { BudgetAnalysis_t} from "./BudgetAnalysis";
 
-const TransactionsTable = ({ transactionData }: { transactionData: Transaction_t[] }) => {
+const TransactionTable = ({ monthData, setMonthData }: { monthData: GetMonthResponse_t, setMonthData: any}) => {
+    const [transactionData, setTransactionData] = useState<Transaction_t[]>(monthData.transactions);
 
     const [showModal, setShowModal] = useState(false);
     const [sortP, setSortP] = useState<{ property: string; direction: "asc" | "desc" }>({
@@ -142,10 +145,121 @@ const TransactionsTable = ({ transactionData }: { transactionData: Transaction_t
                         </Box>
                     </CardBody>
                     <CardFooter align="center" justify="center" pad="medium">
+                       <AddNewTransaction 
+                           transactionsOut={transactionsOut} 
+                           setTransactionsOut={setTransactionsOut}
+                           monthData={monthData}
+                           setMonthData={setMonthData}
+                        />
                     </CardFooter>
                  </Card>
             </> 
-      );
+    );
+};
+
+const AddNewTransaction = ({transactionsOut, setTransactionsOut, monthData, setMonthData} : {transactionsOut: Transaction_t[], setTransactionsOut: any, monthData: GetMonthResponse_t, setMonthData: any}) => {
+    const [addNew, setAddNew] = useState<boolean>(false);
+    const [date, setDate] = useState<string>(new Date().toISOString());
+    const [amount, setAmount] = useState<string>("$0.00");
+    const [category, setCategory] = useState<string>("");
+    const [valid, setValid] = useState<boolean>(false);
+    const categories: string[] = ['Rent', 'Utilities', 'Auto', 'Entertainment', 'Groceries', 'Regular Salary'];
+
+    const tryToParseFloat = (value: string) => {
+        const newValue = parseFloat(value.substring(1));
+        if(value.length == 1){
+            setAmount("$")
+        }
+        if(!isNaN(newValue) && value.split(".").length < 3 && (!isNaN(parseFloat(value.substring(value.length-1))) || value[value.length-1] === '.')){
+            if((value.split(".").length == 2 && value.split(".")[1].length > 2)){
+                setValid(isValidTransaction(value, category));
+                return;
+            }
+            setAmount(value);
+        }
+        setValid(isValidTransaction(value, category));
     };
+
+    const onChange = (event: any) => {
+        const nextValue = event.value;
+        setDate(nextValue);
+        setValid(isValidTransaction(amount, category));
+    };
+
+    const reset = () => {
+        setDate(new Date().toISOString());
+        setAmount("$0.00");
+        setCategory("");
+        setValid(false);
+        setAddNew(false);
+    };
+
+    const setNewCategory = (category:string) => {
+        setCategory(category);
+        setValid(isValidTransaction(amount, category));
+    }
+
+
+    const isValidTransaction = (curAmount: string, curCategory: string) => {
+        if(!categories.includes(curCategory))
+            return false;
+        if(curAmount.length < 2)
+            return false;
+        if(parseFloat(curAmount.substring(1)) <= 0)
+            return false;
+        return true;
+    };
+
+    const addNewTransaction = () => {
+        const newTransaction = new Transaction_t("id", parseFloat(amount.substring(1)), date, "description", category, 1);
+        let newTransactions = [...transactionsOut];
+        newTransactions.push(newTransaction);
+        setTransactionsOut(newTransactions);
+        let newMonthData = {...monthData} as GetMonthResponse_t;
+        newMonthData.transactions = newTransactions;
+        let newBudgetAnalysis = {...monthData.budgetAnalysis}
+        for(let cat of newBudgetAnalysis.budgetOutcomes){
+            if(cat.category === category){
+                cat.outcome += newTransaction.cost;
+            }
+        }
+        newBudgetAnalysis.totalExpense += newTransaction.cost;
+        newMonthData.budgetAnalysis = newBudgetAnalysis;
+        setMonthData(newMonthData);
+        reset();
+    };
+
+    if(!addNew){
+        return(
+            <Button label="Add New Transaction" onClick={() => setAddNew(true)}/>
+        );
+    }
+    return(
+        <Box>
+        <Box direction='row-responsive'>
+            <DateInput
+                format="mm/dd/yyyy"
+                value={date}
+                onChange={event => onChange(event)}
+            />
+            <TextInput
+                placeholder="Transaction Amount"
+                value={amount}
+                onChange={event => tryToParseFloat(event.target.value)}
+            />
+            <Select
+                placeholder="Category"
+                options={categories}
+                value={category}
+                onChange={({option}) => {setNewCategory(option)}}
+            />
+        </Box>
+            <Box direction="row-responsive" alignSelf="center">
+               <Button label="Accept" disabled={!valid} onClick={() => addNewTransaction()}/>
+               <Button label="Cancel" onClick={() => reset()}/>
+            </Box>
+        </Box>
+    );
+};
     
-    export default TransactionsTable;
+export default TransactionTable;
